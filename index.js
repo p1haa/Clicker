fb116279f7a7caaafa306ccf5ff12b8604ce5d8e
pasha <pashutaf@gmail.com>
2026-09-12 15:43:58 -0600

    Very small fixes

--- index.js
+++ index.js
@@ -19,8 +19,16 @@ let upgradectext1 = document.getElementById("upgradectext1");
 
 //Upgrade LVL 100
 let upgradeb100 = document.getElementById("upgrade100");
-let upgradec100 = 100;
+let upgradec100 = 150;
 let upgradectext100 = document.getElementById("upgradectext100");
+
+//Autoclicker and text
+let autob = document.getElementById("autob");
+let autoc = 50;
+let autoctext = document.getElementById("autoctext");
+let autopower = 0;
+let autoptext = document.getElementById("autop")
+
 
 
 //A function to refresh all text
@@ -28,13 +36,15 @@ function allTextRefresh() {
     moneytext.textContent = "Money: " + money + "$";
     upgradectext1.textContent = "Cost: " + upgradec1 + "$";
     upgradectext100.textContent = "Cost: " + upgradec100 + "$";
-    broke.style.display = "none";
     clickptext.textContent = "Click power: " + clickpower + "$ per click";
+    autoctext.textContent = "Cost: " + autoc + "$";
+    autoptext.textContent = "Autoclicker power: " + autopower + "$ per second";
 }
 
 //Adds money and refreshes text
 button.onclick = function () {
     money += clickpower;
+    broke.style.display = "none";1
     allTextRefresh();
 }
 
@@ -69,3 +79,24 @@ upgradeb100.onclick = function () {
         allTextRefresh();
     }
 }
+
+autob.onclick = function () {
+    if (money < autoc) {
+        console.error("NOT ENOUGH MONEY");
+        broke.style.display = "block";
+    }
+    else {
+        autopower += 1;
+        money -= autoc;
+        autoc += Math.floor(50 * (autopower * 0.15));
+        allTextRefresh();
+    }
+}
+
+//Autoclicker function and cycle
+function autoClickerCycle() {
+    money += autopower;
+    allTextRefresh();
+}
+
+setInterval(autoClickerCycle, 1000);
