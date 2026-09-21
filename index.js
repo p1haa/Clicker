00dd4b5091fdae41148a6b5ca7e0b7a439f7ff79
p1haa <pashaf3@educbe.ca>
2026-09-21 15:23:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ let autoptext = document.getElementById("autop");
 //Autoclicker LVL 10
 let autob10 = document.getElementById("autob10");
 let autoc10 = 50;
-let autoctext10 = document.getElementById("autoctext");
+let autoctext10 = document.getElementById("autoctext10");
 
 // GOLDEN BUTTON!!
 let goldenb = document.getElementById("golden");
